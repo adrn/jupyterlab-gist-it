6804a6e57940808606fc1bee749b219166cfbacb
Adrian Price-Whelan <adrian.prw@gmail.com>
2023-09-27 10:47:38 -0400

    detect existing gist

--- src/gistitwidget.ts
+++ src/gistitwidget.ts
@@ -35,17 +35,6 @@ export default class GistItWidget extends Widget {
 
   // Set up notebook metadata:
   let model = this._panel.model;
-  if (model == null) {
-    return;
-  }
-  let gist_info = model.getMetadata('gist_info');
-
-  if (gist_info == undefined) {
-    gist_info = { gist_url: null, gist_id: null, create_date: null, update_date: null};
-    model.setMetadata('gist_info', gist_info);
-  }
-  console.log("gist_info:");
-  console.log(gist_info);
 
   const buttons: Array<[string, ToolbarButton]> = [];
   buttons.push([
@@ -54,12 +43,20 @@ export default class GistItWidget extends Widget {
     className: 'gistItSend',
     iconClass: 'fa fa-github ',
     onClick: async () => {
-      // downloadNotebookFromBrowser(panel);
+      if (model == null) {
+        // TODO: notification error
+        return;
+      }
+      let gist_info = model.getMetadata('gist_info');
+      console.log(gist_info);
+
       console.log(`Sup. ${this._settings.personalAccessToken} stuff`);
       const octokit = new Octokit({ auth: this._settings.personalAccessToken });
 
-      if (gist_info.gist_id == null) {
+      if (gist_info == null || gist_info.gist_id == null) {
         console.log('Creating new gist');
+
+        // TODO: check response code and error handling...
         const resp = await octokit.request('POST /gists', {
           description: 'Example of a gist',
           'public': false,
@@ -73,17 +70,20 @@ export default class GistItWidget extends Widget {
           }
           });
 
-          console.log(resp);
-        // TODO: check response code...
+        console.log(resp);
 
-        Notification.success(`Created a new gist at ${resp.data.html_url}`, {
+        // TODO: error if bad response or response code
+
+        Notification.success(`Created a new gist ${resp.data.id}`, {
             actions: [
+              // @ts-ignore:next-line
+              { label: 'Copy URL', callback: () => navigator.clipboard.writeText(resp.data.html_url) },
               { label: 'Open Gist', callback: () => window.open(resp.data.html_url, '_blank') }
             ],
             autoClose: 5000
           });
 
-        gist_info = { gist_url: resp.data.html_url, gist_id: resp.data.id, create_date: resp.data.created_at, update_date: null};
+        gist_info = { gist_url: resp.data.html_url, gist_id: resp.data.id, create_date: resp.data.created_at};
 
         // @ts-ignore:next-line
         model.setMetadata('gist_info', gist_info);
@@ -91,6 +91,7 @@ export default class GistItWidget extends Widget {
       } else {
         console.log(`gist already exists: ${gist_info.gist_id}`);
 
+        // TODO: check response code and error handling...
         const resp = await octokit.request(`GET /gists/${gist_info.gist_id}`, {
           gist_id: gist_info.gist_id,
           headers: {
@@ -100,6 +101,8 @@ export default class GistItWidget extends Widget {
 
         Notification.warning(`Gist already exists at ${resp.data.html_url}`, {
           actions: [
+            // @ts-ignore:next-line
+            { label: 'Copy URL', callback: () => navigator.clipboard.writeText(resp.data.html_url) },
             { label: 'Open Gist', callback: () => window.open(resp.data.html_url, '_blank') },
             { label: 'Update Gist', callback: () => window.alert("TODO") }
           ],
@@ -107,13 +110,6 @@ export default class GistItWidget extends Widget {
         });
       }
 
-      // Load public gists:
-      // const test = await octokit.request('GET /gists/public', {
-      //     headers: {
-      //       'X-GitHub-Api-Version': '2022-11-28'
-      //     }
-      //   });
-      // console.log(test);
     },
     tooltip: 'Gist It',
     // label: 'GistIt',
